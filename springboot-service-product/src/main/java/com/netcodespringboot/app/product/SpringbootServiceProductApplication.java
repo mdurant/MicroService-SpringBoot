d787b8cfa7185e64c75307eccf832df0d24237b4
MAURICIO DURAN T <mauriciodurant@gmail.com>
2019-07-22 00:25:31 -0400

    Proyecto inicio Java Spring Boot

---
+++ springboot-service-product/src/main/java/com/netcodespringboot/app/product/SpringbootServiceProductApplication.java
@@ -0,0 +1,13 @@
+package com.netcodespringboot.app.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootServiceProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootServiceProductApplication.class, args);
+	}
+
+}
---
+++ springboot-service-product/src/test/java/com/netcodespringboot/app/product/SpringbootServiceProductApplicationTests.java
@@ -0,0 +1,16 @@
+package com.netcodespringboot.app.product;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootServiceProductApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
